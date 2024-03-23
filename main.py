612110a8b9489b2c40add2a8cd1170c5fe579865
suraj8300 <161626788+suraj8300@users.noreply.github.com>
2024-03-23 11:51:13 +0530

    Add two numbers

---
+++ main.py
@@ -0,0 +1,15 @@
+# Hello this is Suraj. We will look here program to add two number.
+
+# Add two Numbers
+
+# Defining the function named add which will add two numbers.
+def add():
+    p = a + b
+    print(p)
+
+# Taking two inputs from user and assigning them to a and b.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Calling function add to add the two numbers
+add() 
